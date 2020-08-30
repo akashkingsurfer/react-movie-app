aa596abd39095a8b255f27437afb26b9365f7e5b
akash <akashkingsurfer241@gmail.com>
2020-08-30 08:17:33 +0530

    main folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Welcome from './components/Welcome';
+import Greet from './components/class1'
+
+
+function App() {
+  return (
+    <h1>hello workd</h1>
+  );
+}
+
+export default App;
